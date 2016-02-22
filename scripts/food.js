26beb9c319ff5b3f9415ba32356ba5bd3bd16c21
bharding2 <b.harding@gmail.com>
2016-02-22 11:07:29 -0800

    integrated socrata api token and king county proxy into server.js, added ajax call on food.js

--- scripts/food.js
+++ scripts/food.js
@@ -3,10 +3,18 @@
   inspection.all =[];
 
   inspection.requestInspectionData = function(callback) {
-    $.get('https://data.kingcounty.gov/resource/gkhn-e8mn.json')
-      .done(function(data, message, xhr) {
-        inspection.all = data;
-      }).done(callback);
+    // $.get('https://data.kingcounty.gov/resource/gkhn-e8mn.json')
+    //   .done(function(data, message, xhr) {
+    //     inspection.all = data;
+    //   }).done(callback);
+    $.ajax({
+      type: 'GET',
+      url: 'data/resource/gkhn-e8mn.json?$query=SELECT%20name,inspection_score,inspection_date%20ORDER%20BY%20name,inspection_date%20DESC',
+      success: function(data, message, xhr){
+        console.log(xhr);
+        console.log(data);
+      }
+    });
   };
 
   inspection.with = function(attr) {
