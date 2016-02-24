50451de273f3968b48ee3303d0886edb2b654f64
Katherine Beame <katherine.beame@gmail.com>
2016-02-24 13:20:53 -0800

    created filter funciton in inspecitonView.js for score analysis

--- scripts/inspectionView.js
+++ scripts/inspectionView.js
@@ -2,16 +2,28 @@
   var inspectionView = {};
 
   inspectionView.displayResults = function(inspection) {
-    // var $reportCard = $('#report-card ul');
-    //
-    // $reportCard.empty();
-    // $reportCard.append($('<li>').text('Restaurant Name: ' + Inspection.current[0].name));
-    // $reportCard.append($('<li>').text('Violation Score: ' + Inspection.current[0].inspection_score));
-    // $reportCard.append($('<li>').text('Date: ' + Inspection.current[0].inspection_date.substring(0, 10)));
-    // $('#report-card').css('background-color', 'green');
     var template = Handlebars.compile($('#report-card-template').text());
 
     return template(inspection);
+  };
+  inspectionView.filterResults = function(result) {
+    if (result.inspection_score <= 5) {
+      $('results-main').attr('class', 'low');
+      $('.score img').attr('src', 'images/inspection-grade-png/doodle-smiley-icons-a.png');
+    } else if (result.inspection_score <= 35) {
+      $('results-main').attr('class', 'moderate');
+      $('.score img').attr('src', 'images/inspection-grade-png/doodle-smiley-icons-b-plus.png');
+    } else if (result.inspection_score <= 45) {
+      $('results-main').attr('class', 'elevated');
+      $('.score img').attr('src', 'images/inspection-grade-png/doodle-smiley-icons-b.png');
+    } else if (result.inspection_score <= 90) {
+      $('results-main').attr('class', 'high');
+      $('.score img').attr('src', 'images/inspection-grade-png/doodle-smiley-icons-c.png');
+    } else {
+      $('results-main').attr('class', 'severe');
+      $('.score img').attr('src', 'images/inspection-grade-png/doodle-smiley-icons-f.png');
+    }
+
   };
 
   module.inspectionView = inspectionView;
