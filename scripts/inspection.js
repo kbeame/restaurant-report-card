dabe4e863476cfca0f7b7f303375fb270316c63b
bharding2 <b.harding@gmail.com>
2016-02-24 13:04:55 -0800

    handlebars compile and render to index

--- scripts/inspection.js
+++ scripts/inspection.js
@@ -59,8 +59,8 @@
           //cache the data into the currently empty array
           total.insertData();
         });
+        $('#report-card').empty().append(inspectionView.displayResults(Inspection.current[0]));
         mapView.updateMap();
-        inspectionView.displayResults();
       }).done(callback);
   };
 
