8584ed482de0fd77bfd30f2cee21d8efca6cd5d6
Katherine Beame <katherine.beame@gmail.com>
2016-02-24 13:28:23 -0800

    changed the result-main class to an id

--- scripts/inspectionView.js
+++ scripts/inspectionView.js
@@ -8,19 +8,19 @@
   };
   inspectionView.filterResults = function(result) {
     if (result.inspection_score <= 5) {
-      $('results-main').attr('class', 'low');
+      $('#results-main').attr('class', 'low');
       $('.score img').attr('src', 'images/inspection-grade-png/doodle-smiley-icons-a.png');
     } else if (result.inspection_score <= 35) {
-      $('results-main').attr('class', 'moderate');
+      $('#results-main').attr('class', 'moderate');
       $('.score img').attr('src', 'images/inspection-grade-png/doodle-smiley-icons-b-plus.png');
     } else if (result.inspection_score <= 45) {
-      $('results-main').attr('class', 'elevated');
+      $('#results-main').attr('class', 'elevated');
       $('.score img').attr('src', 'images/inspection-grade-png/doodle-smiley-icons-b.png');
     } else if (result.inspection_score <= 90) {
-      $('results-main').attr('class', 'high');
+      $('#results-main').attr('class', 'high');
       $('.score img').attr('src', 'images/inspection-grade-png/doodle-smiley-icons-c.png');
     } else {
-      $('results-main').attr('class', 'severe');
+      $('#results-main').attr('class', 'severe');
       $('.score img').attr('src', 'images/inspection-grade-png/doodle-smiley-icons-f.png');
     }
   };
