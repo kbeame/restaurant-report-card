7c4de491b2a187ffb0b0fba30be569ff11a38d75
bharding2 <b.harding@gmail.com>
2016-02-24 10:01:03 -0800

    truncated date to 10 char

--- scripts/inspectionView.js
+++ scripts/inspectionView.js
@@ -7,7 +7,7 @@
     $reportCard.empty();
     $reportCard.append($('<li>').text('Restaurant Name: ' + Inspection.current[0].name));
     $reportCard.append($('<li>').text('Violation Score: ' + Inspection.current[0].inspection_score));
-    $reportCard.append($('<li>').text('Violation Score: ' + Inspection.current[0].inspection_date));
+    $reportCard.append($('<li>').text('Date: ' + Inspection.current[0].inspection_date.substring(0, 10)));
     $('#report-card').css('background-color', 'green');
   };
 
