4710b381b63b780e74eeeaa30e504fb0b087cd6a
Katherine Beame <katherine.beame@gmail.com>
2016-02-24 09:54:40 -0800

    commented out the historyView.displayResults function

---
+++ scripts/history.js
@@ -0,0 +1,16 @@
+(function(module) {
+  var history = {};
+  history.all = [];
+
+  history.requestHistoryData = function(place, callback) {
+    $.get('/data/resource/gkhn-e8mn.json?$select=name,inspection_date,inspection_score,address,inspection_result,zip_code,city,inspection_closed_business,violation_type,violation_description,longitude,latitude&$order=inspection_date%20DESC&inspection_type=Routine%20Inspection/Field%20Review&name=' + place)
+    .done(function(data, message, xhr) {
+      history.all = data;
+      // historyView.displayResults();
+    }).done(callback);
+  };
+
+
+
+  module.history = history;
+})(window);
