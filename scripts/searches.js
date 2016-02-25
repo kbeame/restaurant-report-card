92173229e6db6824f368ca8040f2d4c671ec35f4
bharding2 <b.harding@gmail.com>
2016-02-25 09:11:09 -0800

    IIFEs and script tags

---
+++ scripts/searches.js
@@ -0,0 +1,6 @@
+(function(module) {
+  var searches = {};
+
+
+  module.searches = searches;
+})(window);
---
+++ scripts/searchesView.js
@@ -0,0 +1,6 @@
+(function(module) {
+  var searchesView = {};
+
+
+  module.searchesView = searchesView;
+})(window);
