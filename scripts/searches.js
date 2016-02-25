2532159fcea52b29b2726ca9290f9d536e62224f
bharding2 <b.harding@gmail.com>
2016-02-25 09:24:25 -0800

    starting view and handler functions

--- scripts/searches.js
+++ scripts/searches.js
@@ -1,6 +1,12 @@
 (function(module) {
   var searches = {};
 
+  searches.init = function () {
+    webDB.execute('SELECT * FROM recentInspect', function(rows) {
+      
+    });
+  };
+  searches.init();
 
   module.searches = searches;
 })(window);
--- scripts/searchesView.js
+++ scripts/searchesView.js
@@ -1,6 +1,11 @@
 (function(module) {
   var searchesView = {};
 
+  searchesView.displaySearchRow = function(result) {
+    var template = Handlebars.compile($('#history-template').text());
+
+    return template(result);
+  };
 
   module.searchesView = searchesView;
 })(window);
