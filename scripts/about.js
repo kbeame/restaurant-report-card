5fdedd0427d2e8f311d4615e0b248d67cd70f0f9
bharding2 <b.harding@gmail.com>
2016-02-25 10:25:53 -0800

    konami effects, click effects, IIFE

--- scripts/about.js
+++ scripts/about.js
@@ -1,5 +1,14 @@
-$( window ).konami();
-$( window ).on('konami', function() {
-  $('#about-bear').attr('class', 'in-view');
-  $('#about-glasses').attr('class', 'slide-down');
-});
+(function(module) {
+  var aboutInit = {};
+
+  aboutInit.aboutKonami = function () {
+    $( window ).konami();
+    $( window ).on('konami', function() {
+      $('#about-bear').attr('class', 'in-view');
+      $('#about-glasses').attr('class', 'slide-down');
+    });
+  };
+  aboutInit.aboutKonami();
+
+  module.aboutInit = aboutInit;
+})(window);
--- scripts/aboutController.js
+++ scripts/aboutController.js
@@ -3,6 +3,10 @@
 
   aboutController.index= function() {
     $('#about').show().siblings().hide();
+    $('#about-glasses').removeClass('slide-down');
+    setTimeout(function () {
+      $('#about-bear').removeClass('in-view');
+    }, 3000);
   };
   module.aboutController = aboutController;
 })(window);
