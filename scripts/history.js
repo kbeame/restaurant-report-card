7a543896cc43f81ea2daea78903349c7a5d151fc
bharding2 <b.harding@gmail.com>
2016-02-25 11:54:53 -0800

    truncate date on history.js

--- scripts/history.js
+++ scripts/history.js
@@ -9,6 +9,7 @@
       // $('#inspection-history').show();
       $('#inspection-history table tbody').empty();
       historyInit.all.forEach(function(current){
+        current.inspection_date = current.inspection_date.substring(0, 10);
         $('#inspection-history table tbody').append(historyView.displayHistoryRow(current));
       });
     });
