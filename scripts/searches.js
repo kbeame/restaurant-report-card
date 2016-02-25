272f7454dfca11a2e0cd24cd58c2881a6f1e5b41
bharding2 <b.harding@gmail.com>
2016-02-25 09:36:18 -0800

    database access, row display

--- scripts/searches.js
+++ scripts/searches.js
@@ -3,7 +3,10 @@
 
   searches.init = function () {
     webDB.execute('SELECT * FROM recentInspect', function(rows) {
-      
+      $('#search-history table tbody').empty();
+      rows.forEach(function(current){
+        $('#search-history table tbody').append(searchesView.displaySearchRow(current));
+      });
     });
   };
   searches.init();
